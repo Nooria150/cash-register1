d71d784f2a21c75b3bd36aad5ed82a3691942da2
Nooria150 <nooriamangal55@gmail.com>
2024-07-10 10:44:46 -0700

    solved error

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-let price = 19.5;
-let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100]
+const price = 19.5;
+const cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE"', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
 ];
 
 document.getElementById('purchase-btn').addEventListener('click', calculateChange);
